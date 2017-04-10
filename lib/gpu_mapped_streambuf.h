8a499c2d37c52980c904718b780e64ab5a7a9bed
◄hollsteinm► <hollsteinm@users.noreply.github.com>
2017-04-10 09:56:11 -0500

    Update gpu_mapped_streambuf.h
    Added new sync overload and updated class name to match file

--- lib/gpu_mapped_streambuf.h
+++ lib/gpu_mapped_streambuf.h
@@ -7,22 +7,23 @@
 namespace gams {
     class igpu_mem_buffer;
 
-    class gpu_streambuf : public std::streambuf
+    class gpu_mapped_streambuf : public std::streambuf
     {
     public: 
-        gpu_streambuf(igpu_mem_buffer* gpu_buffer);
-        virtual ~gpu_streambuf();
+        gpu_mapped_streambuf(igpu_mem_buffer* gpu_buffer);
+        virtual ~gpu_mapped_streambuf();
 
         virtual std::streambuf::int_type underflow() override;
         virtual std::streambuf::int_type overflow(std::streambuf::int_type value) override;
 
-		gpu_streambuf(const gpu_streambuf&) = delete;
-		gpu_streambuf& operator=(const gpu_streambuf&) = delete;
+	gpu_mapped_streambuf(const gpu_mapped_streambuf&) = delete;
+	gpu_mapped_streambuf& operator=(const gpu_mapped_streambuf&) = delete;
 
-	protected:
-		virtual std::streambuf::int_type sync() override;
+    protected:
+        virtual std::streambuf::int_type sync() override;
+	std::streambuf::int_type sync(bool prepare_after_commit);
 
     private:
-		igpu_mem_buffer* gpu_buffer;
+        igpu_mem_buffer* gpu_buffer;
     };
 }
